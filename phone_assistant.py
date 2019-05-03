12d7ccfcc04f1efd47d5b0b97d565014b9a05f03
Mazzart <am.magicsmile@gmail.com>
2019-05-04 00:51:17 +0300

    Add the following methods: create_connection - creates a connection to the database regexp - REGEXP implementation for SQLite using Python function read_from_db - query up to 10 rows that match the regular expression and returns a list of them
    New function calls in the main function. Clean up code

--- phone_assistant.py
+++ phone_assistant.py
@@ -2,22 +2,42 @@
 
 import argparse
 import re
+import sqlite3
 
-PHONE_NUMBERS = [380675674432, 380672832500, 380983567721]
+
+def create_connection():
+    """Create a database connection"""
+
+    try:
+        conn = sqlite3.connect('phones.db')
+        return conn
+    except sqlite3.Error as error:
+        print(error)
+
+    return None
 
 
-def phone_number_search(argv):
-    """Search for valid phone numbers"""
+def regexp(expr, item):
+    """Regular expression function"""
 
-    correct_phone_numbers = []
-    phone_number_regex = argv + r'[0-9]{' + str(12-len(argv)) + r'}$'
-    phone_number_pattern = re.compile(phone_number_regex)
+    reg = re.compile(expr)
+    return reg.search(item) is not None
 
-    for item in PHONE_NUMBERS:
-        if phone_number_pattern.match(str(item)):
-            correct_phone_numbers.append(item)
 
-    return correct_phone_numbers
+def read_from_db(conn, argv):
+    """Query rows that match the regular expression"""
+
+    conn.create_function('REGEXP', 2, regexp)
+    cur = conn.cursor()
+
+    argv = str(argv.integer)
+    phone_number_regex = argv + r'[0-9]{' + str(12 - len(argv)) + r'}$'
+    cur.execute("SELECT phone_number FROM phones WHERE phone_number REGEXP ? LIMIT 10",
+                (phone_number_regex,))
+
+    rows = cur.fetchall()
+
+    return [int(x[0]) for x in rows]
 
 
 def arg_parse():
@@ -35,7 +55,8 @@ def main():
     """Main entry point"""
 
     argv = arg_parse().parse_args()
-    result = phone_number_search(str(argv.integer))
+    conn = create_connection()
+    result = read_from_db(conn, argv)
     print(result)
 
 
