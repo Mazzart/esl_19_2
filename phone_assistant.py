7ecb78a3dfd23ea830ae236ea8c8bd3fdf305054
Mazzart <am.magicsmile@gmail.com>
2019-05-03 13:59:31 +0300

    Add the following methods: arg_parse - to initialize argument parser main - which is a main entry point to the program

--- phone_assistant.py
+++ phone_assistant.py
@@ -1,4 +1,24 @@
 """Phone assistant to search for a phone number in the database"""
 
+import argparse
+
 PHONE_NUMBERS = [380675674432, 380672832500, 380983567721]
 
+
+def arg_parse():
+    """Initialize argument parser"""
+
+    arg_parser = argparse.ArgumentParser(description='Process integer input')
+    arg_parser.add_argument('integer',
+                            type=int,
+                            help='An integer input to search for a phone number')
+
+    return arg_parser
+
+
+def main():
+    """Main entry point"""
+
+
+if __name__ == '__main__':
+    main()
