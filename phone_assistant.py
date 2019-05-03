4dd1f00cf34120b859c3d6692dbe8e2b795c6026
Mazzart <am.magicsmile@gmail.com>
2019-05-03 15:20:39 +0300

    Add method phone_number_search which search for valid phone numbers using regular expression

--- phone_assistant.py
+++ phone_assistant.py
@@ -1,8 +1,23 @@
 """Phone assistant to search for a phone number in the database"""
 
 import argparse
+import re
 
 PHONE_NUMBERS = [380675674432, 380672832500, 380983567721]
+
+
+def phone_number_search(argv='38067'):
+    """Search for valid phone numbers"""
+
+    correct_phone_numbers = []
+    phone_number_regex = re.escape(argv) + r'[0-9]{' + str(12-len(argv)) + r'}$'
+    phone_number_pattern = re.compile(phone_number_regex)
+
+    for item in PHONE_NUMBERS:
+        if phone_number_pattern.match(str(item)):
+            correct_phone_numbers.append(item)
+
+    return correct_phone_numbers
 
 
 def arg_parse():
